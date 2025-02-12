6e91a7601b9882f10e3341d13a024073c2ac9455
k-ghale <kabinghale888@gmail.com>
2025-02-11 22:54:39 -0500

    assignment 2 done

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,2 +1,13 @@
+"use strict";
 
-console.log('hello world');
+window.AddEventListener("load",setupGallery);
+
+function setupGallery() {
+    let imgCount = imgFiles.Length;
+    let galleryBox = document.getElementById("galleryBox");
+    let currentSlide = 1;
+    let isRunning = false;
+    let showRunning;
+    
+
+}
